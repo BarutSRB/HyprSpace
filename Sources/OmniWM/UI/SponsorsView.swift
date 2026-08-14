e4f13086fd6cc0f53abe4303a148b594258ae55a
Barut <12249659+BarutSRB@users.noreply.github.com>
2026-08-14 00:39:45 -0400

    Update: Sponsors

--- Sources/OmniWM/UI/SponsorsView.swift
+++ Sources/OmniWM/UI/SponsorsView.swift
@@ -30,7 +30,9 @@ private let sponsors: [Sponsor] = [
     Sponsor(name: "benhaotang", githubUsername: "benhaotang", imageName: "benhaotang", imageExtension: "png"),
     Sponsor(name: "Chris M", githubUsername: "tebriel", imageName: "tebriel", imageExtension: "jpg"),
     Sponsor(name: "marckeelingiv", githubUsername: "marckeelingiv", imageName: "marckeelingiv", imageExtension: "png"),
-    Sponsor(name: "Nader Akoury", githubUsername: "dojoteef", imageName: "dojoteef", imageExtension: "jpg")
+    Sponsor(name: "Nader Akoury", githubUsername: "dojoteef", imageName: "dojoteef", imageExtension: "jpg"),
+    Sponsor(name: "Earl Gresh", githubUsername: "earl-gresh", imageName: "earl-gresh", imageExtension: "jpg"),
+    Sponsor(name: "Carson Full", githubUsername: "CarsonF", imageName: "carsonf", imageExtension: "jpg")
 ]
 
 private func rankLabel(for index: Int) -> String {
